1fb46150143edf081ff5aa02099b3d50b33634fa
Maksym Lytvynchuk <maksym@trynow.io>
2021-02-19 16:26:20 +0200

    Fixed tests

--- project/accounts/services.py
+++ project/accounts/services.py
@@ -1,14 +1,17 @@
+from decimal import Decimal
+
 from .models import Account, Transaction
 
 
 def perform_deposit(user, amount):
     account = Account.objects.get(user=user)
-    account.balance += amount
+    account.balance += Decimal(amount)
     account.save()
 
     transaction = Transaction.objects.create(
         account=account,
-        transaction_type=Transaction.TRANSACTION_TYPE_DEPOSIT
+        transaction_type=Transaction.TRANSACTION_TYPE_DEPOSIT,
+        amount=amount
     )
 
     return transaction
--- project/accounts/tests.py
+++ project/accounts/tests.py
@@ -1,5 +1,7 @@
-from django.test import TestCase
+from decimal import Decimal
+
 from django.contrib.auth.models import User
+from django.test import TestCase
 
 from .models import Account, Transaction
 from .services import perform_deposit
@@ -13,7 +15,7 @@ class DepositTestCase(TestCase):
     def test_single_deposit(self):
         # Get the account and check 0 balance
         account = Account.objects.get(user=self.user)
-        self.assertEquals(account.balance, 0)
+        self.assertEquals(account.balance, Decimal('0'))
         # Check 0 transactions in database
         self.assertEquals(Transaction.objects.count(), 0)
 
@@ -24,7 +26,7 @@ class DepositTestCase(TestCase):
         self.assertEquals(Transaction.objects.count(), 1)
         # Check balance is now 0.1
         account = Account.objects.get(user=self.user)
-        self.assertEquals(account.balance, 0.1)
+        self.assertEquals(account.balance, Decimal('0.1'))
 
     def test_multiple_deposit(self):
         # Get the account and check 0 balance
@@ -42,4 +44,4 @@ class DepositTestCase(TestCase):
         self.assertEquals(Transaction.objects.count(), 3)
         # Check balance is now 0.1
         account = Account.objects.get(user=self.user)
-        self.assertEquals(account.balance, 0.3)
+        self.assertEquals(account.balance, Decimal('0.3'))
